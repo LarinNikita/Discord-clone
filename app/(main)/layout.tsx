7115754c93c4fd4730b8c7eb61d93e818a5c43b4
LarinNikita <larin_nikita_m@mail.ru>
2024-01-27 13:12:06 +0700

    dark & light theme setup

---
+++ app/(main)/layout.tsx
@@ -0,0 +1,13 @@
+interface MainLayoutProps {
+    children: React.ReactNode,
+};
+
+const MainLayout = ({ children }: MainLayoutProps) => {
+    return (
+        <div>
+            {children}
+        </div>
+    );
+};
+
+export default MainLayout;
