033f9ab984de1b9c5b5129821cfc2be6190dc5c2
LarinNikita <larin_nikita_m@mail.ru>
2024-02-24 16:23:33 +0700

    chat scroll hook

--- components/chat/chat-messages.tsx
+++ components/chat/chat-messages.tsx
@@ -7,6 +7,7 @@ import { format } from 'date-fns';
 
 import { useChatQuery } from '@/hooks/use-chat-query';
 import { useChatSocket } from '@/hooks/use-chat-socket';
+import { useChatScroll } from '@/hooks/use-chat-scroll';
 
 import { ChatWelcome } from './chat-welcome';
 import { ChatItem } from './chat-item';
@@ -53,6 +54,13 @@ export const ChatMessages = ({
         useChatQuery({ queryKey, apiUrl, paramKey, paramValue });
 
     useChatSocket({ queryKey, addKey, updateKey });
+    useChatScroll({
+        chatRef,
+        bottomRef,
+        loadMore: fetchNextPage,
+        shouldLoadMore: !isFetchingNextPage && !!hasNextPage,
+        count: data?.pages?.[0]?.items?.length ?? 0,
+    });
 
     if (status === 'loading') {
         return (
